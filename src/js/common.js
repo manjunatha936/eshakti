40450357d1e24aadbe10dc3311d7ad2eb6802ae9
Manjunath <radhamani@lollypop.biz>
2020-02-12 13:05:55 +0530

    footer

--- src/js/common.js
+++ src/js/common.js
@@ -6,7 +6,11 @@ import "../pug/index.pug";
 import "../pug/about.pug";
 import "../pug/contact.pug";
 
-import "./components/header";
+
+import HeaderInteractions from "./components/header";
+const headerfns = new HeaderInteractions();
+// import "./components/header";
+
 
 console.log("Hi, this page is common page!"); // eslint-disable-line no-console
 
