507281da628be1d29822ff02f0e1089b8056f729
unknown <vinod@lollypop.design>
2020-02-26 11:22:25 +0530

    product listing update

--- src/js/common.js
+++ src/js/common.js
@@ -1,14 +1,15 @@
 import "../scss/main.scss";
-
+import $ from 'jquery'
 // import "@babel/polyfill";
 import "bootstrap/js/src/modal";
+import "bootstrap/js/src/tab";
+// import "bootstrap/js/src/dropdown";
 
 
 import "../pug/index.pug";
 import "../pug/repeat-customer.pug";
-import "../pug/about.pug";
+// import "../pug/about.pug";
 import "../pug/contact.pug";
-
 
 import HeaderInteractions from "./components/header";
 const headerfns = new HeaderInteractions();
