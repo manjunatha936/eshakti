1fab37cdb828a696ad95b52a292e13d5379d21a6
Manjunath <radhamani@lollypop.biz>
2020-02-27 16:11:25 +0530

    filters updated

--- src/js/about.js
+++ src/js/about.js
@@ -1,26 +1,129 @@
-// import $ from 'jquery';
+import $ from 'jquery';
+// import './plugins/jquery-ui.min';
+// import './plugins/jquery.mobile';
 // import 'hammerjs';
+// import 'jquery-touch-events';
+
+$(document).ready(function(){
+    alert(1);
+    $('.content').on('tab', function(e) { 
+        console.log('User tapped #myElement'); 
+    });
+});
+// // var position = 0;
+
+// $(".scroll-div").draggable({
+//     axis: "y",
+//     revert: function() {
+//         // console.log(this);
+//         // if($(this).offset().top < 450) {
+//         //     // alert(1);
+//         //     $(".scroll-div").animate({top: 0}, 300).addClass('active');
+//         //     return false
+//         // }
+//         // else {
+//         //     // alert($(this).offset().top);
+//         //     return true
+//         // }
+//     },
+//     containment: ".main-div",
+//     stop: function(ev, ui) {
+//         if(this.offsetTop < 450) {
+//             $(".scroll-div").animate({top: 0}, 300).addClass('active');
+//         }
+//         else {
+//             $(".scroll-div").animate({top: 510}, 300);
+//         }
+//     },
+//     drag: function(ev, ui) {
+//         if(this.offsetTop > 510 | this.offsetTop < 0) {
+//             return false
+//         }
+//         if(this.offsetTop > 0 && $('.scroll-div').hasClass('active')) {
+//             $(".scroll-div").animate({top: 510}, 300).removeClass('active');
+//         }
+//     },
+//     // drag: function(ev, ui) {
+//         // if (ui.originalPosition.top - ui.offset.top > 0) {
+//         //     alert(1);
+//         // }
+//         // console.log(ev);
+//         // if(ev.clientY > 450) {
+//         //     $(this).css('top', 0).addClass('active');
+//         // }
+//         // else {
+//         //     $(this).animate({top: 510}, 300);
+//         // }
+//         // if (ui.originalPosition.top - ui.offset.top < 0) {
+//         //     return false;
+//         // }
+//         // else {
+//         //     return true
+//         // }
+//         // alert(1);
+//         // if (ui.originalPosition.top - ui.offset.top < 0) {
+//         //     console.log('Dragged Down');
+//         //     if($('.content').scrollTop() <= 0) {
+//         //         $(".scroll-div").css('top', 'calc(100% - 100px)').removeClass('active');
+//         //     }
+//         // } else {
+//         //     console.log('Dragged Up');
+//         //     // if ($(".scroll-div").hasClass('active')) {
+//         //     //     return;
+//         //     // }
+//         //     // else {
+                
+//         //     // }
+//         // }
+//     // }
+// });
 
 // // // Get a reference to an element.
 // var square = document.querySelector('.scroll-div');
+// var content = document.querySelector('.content');
+// var body = document.querySelector('html');
 
 // var touchControl = new Hammer(square);
+// var touchContent = new Hammer(content);
+// // touchControl = new Hammer.Manager(square, {
+// //     touchAction: 'auto',
+// //     recognizers: [
+// //         [Hammer.Pan,{ direction: Hammer.DIRECTION_HORIZONTAL }],
+// //     ]
+// // });
 // touchControl.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
+// touchContent.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
 // touchControl.on("panstart", dragElement)
 //             .on("panend", resetElement);
 
-// function dragElement(event) {
-//     if(event.offsetDirection == 16) {
+// touchContent.on("panstart", function(e){
+//     if(e.offsetDirection == 16) {
+//         square.classList.remove('mystyle');
+//         body.classList.remove("overflow-hidden");
 //         square.style.top = 'calc(100% - 100px)';
 //     }
 //     else {
-//         if(event.deltaY < 0 && event.deltaY > -150) {
+//         alert(1);
+//         // var x = content.scrollTop;
+//         // content.animate({scrollTop:x + 50}, 500, 'swing');
+//     }
+// });
+
+// function dragElement(event) {
+//     if(event.offsetDirection == 16) {
+//         square.classList.remove('mystyle');
+//         body.classList.remove("overflow-hidden");
+//         square.style.top = 'calc(100% - 100px)';
+//     }
+//     else if (!square.classList.contains('mystyle')){
+//         if(event.deltaY < 0 && event.deltaY > -120) {
 //             square.style.top = 'calc(100% + ' + event.deltaY + 'px - 100px)';
 //         }
     
-//         if((event.deltaY) < -150) {
+//         if((event.deltaY) < -120) {
 //             square.style.top = 0;
 //             square.classList.add("mystyle");
+//             body.classList.add("overflow-hidden");
 //         }
 //     }
     
@@ -29,11 +132,20 @@
 // function resetElement() {
 //     if(event.offsetDirection == 16) {
 //         square.style.top = 'calc(100% - 100px)';
+//         square.classList.remove('mystyle');
+//         body.classList.remove("overflow-hidden");
 //     }
 
 //     else if(!square.classList.contains('mystyle')) {
-//             // alert('reset');
-//             square.style.top = 'calc(100% - 100px)';
-//         }
-//     // }
+//         square.style.top = 'calc(100% - 100px)';
+//     }
 // };
+
+// $('.content').on('scroll', function() {
+//     if($(this).scrollTop() <= 0) {
+//         $(this).addClass('attop');
+//     }
+//     else {
+//         $(this).removeClass('attop');
+//     }
+// })
--- src/js/common.js
+++ src/js/common.js
@@ -1,12 +1,15 @@
 import "../scss/main.scss";
-
+import $ from 'jquery'
 // import "@babel/polyfill";
+import "bootstrap/js/src/modal";
+import "bootstrap/js/src/tab";
+// import "bootstrap/js/src/dropdown";
 // import "bootstrap/js/src/modal"
 
 
 import "../pug/index.pug";
 import "../pug/repeat-customer.pug";
-import "../pug/about.pug";
+// import "../pug/about.pug";
 import "../pug/contact.pug";
 import "../pug/productlisting.pug";
 
@@ -14,6 +17,8 @@ import "../pug/productlisting.pug";
 import HeaderInteractions from "./components/header";
 const headerfns = new HeaderInteractions();
 // import "./components/header";
+import ProductDetais from './product-listing'
+const productDetais = new ProductDetais();
 
 console.log("Hi, this page is common page!"); // eslint-disable-line no-console
 
