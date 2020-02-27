1bf7ab2fe22db03c7785d64ab6fe34cd6db3ebe2
Manjunath <radhamani@lollypop.biz>
2020-02-27 10:55:02 +0530

    product-listing filter

--- src/js/productlisting.js
+++ src/js/productlisting.js
@@ -23,3 +23,57 @@ gridViewButton.addEventListener('click', () => {
   list.classList.remove('list-item');
   list.classList.add('grid-item');
 });
+
+// Color select 
+
+const clrhightlight = document.querySelectorAll('.color-list__item');
+const colorspan = document.querySelectorAll('.color-circle');
+
+clrhightlight.addEventListener('click', () => {
+  if (colorspan.classList.contains('active')) {
+    colorspan.classList.remove('active');
+    console.log("hell0");
+  } 
+  else {
+    colorspan.classList.add('active');
+    console.log("hello2");
+  }
+  console.log("hello3");
+});
+
+// accordian info
+
+class Accordion {
+   constructor(heading) {
+      this.heading = heading;
+   }
+   
+   showOne() {
+      const accordionHeading = document.querySelectorAll(this.heading);
+      accordionHeading.forEach((item, key) => {
+         item.addEventListener('click', () => { 
+            accordionHeading.forEach(element => {
+               element.classList.contains('active') ? 
+                  element.classList.remove('active') : null;
+            });
+            item.classList.add('active');
+         });
+      });
+   }
+   
+   showAll() {
+      const accordionHeading = document.querySelectorAll(this.heading);
+      accordionHeading.forEach((item, key) => {
+         item.addEventListener('click', () => { 
+            item.classList.contains('active') ? 
+               item.classList.remove('active') :            
+               item.classList.add('active');
+         });
+        //  console.log(item);
+      });
+   }
+}
+
+const accordion = new Accordion('.panel-cardtitle');
+// for open every use showAll();
+accordion.showAll();
