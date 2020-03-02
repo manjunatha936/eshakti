311e80fecf1daf2968396c39d211553c7baa6579
Manjunath <radhamani@lollypop.biz>
2020-03-02 11:12:30 +0530

    updated with listing page

--- src/js/productlisting.js
+++ src/js/productlisting.js
@@ -27,6 +27,7 @@ gridViewButton.addEventListener('click', () => {
 // Color select 
 
 let colorPicker = document.querySelectorAll('.color-list__item');
+let colorClear = document.querySelector('.clear-color');
 // let colorPicker = Array.from(document.querySelectorAll('.color-list__item'));
 
 const handleClick = (e) => {
@@ -35,30 +36,49 @@ const handleClick = (e) => {
     node.classList.remove('active');
   });
   e.currentTarget.classList.add('active');
-  
 }
+
 colorPicker.forEach(node => {
   node.addEventListener('click', handleClick)
 });
 
+colorClear.addEventListener('click', (event) => { 
+   colorPicker.forEach(node => {
+      event.stopPropagation();
+      node.classList.remove('active');
+   })
+})
+
 
 // Product alert
 
-let productAlert = document.querySelectorAll('.neck-wrapper');
-let alertContent = document.querySelectorAll('.product-alert');
+let alertContent = document.querySelector('.product-alert');
+const checkbox = document.querySelectorAll('.style-check');
+const clear = document.querySelector('.clear-select');
 
-const checkbox = document.getElementById('myCheckbox') 
-checkbox.addEventListener('change', (event) => { 
-   if (event.target.checked) { 
-      alert('checked'); 
-   } 
-   else { 
-      alert('not checked'); 
-   } 
+checkbox.forEach(node => {
+   node.addEventListener('change', (event) => { 
+      if (event.target.checked) { 
+         alertContent.classList.add('class-cheked');
+         event.target.nextElementSibling.children
+         alert(event.target.nextElementSibling.children);
+      }
+   })
+});
+
+
+//Clear the selcted list
+
+clear.addEventListener('click', (el) => { 
+   checkbox.forEach(node => {
+      el.stopPropagation();
+      node.checked = false;
+      alertContent.classList.remove('class-cheked');
+   });
 })
 
+ 
 // accordian info
-
 class Accordion {
    constructor(heading) {
       this.heading = heading;
@@ -80,7 +100,7 @@ class Accordion {
    showAll() {
       const accordionHeading = document.querySelectorAll(this.heading);
       accordionHeading.forEach((item, key) => {
-         item.addEventListener('click', () => { 
+         item.addEventListener('click', (event) => {
             item.classList.contains('active') ? 
                item.classList.remove('active') :            
                item.classList.add('active');
