35765d83db64b421b1e167e6055f7442a661a542
unknown <vinod@lollypop.design>
2020-03-20 12:08:36 +0530

    info icon update

--- src/js/product-details/product-details.js
+++ src/js/product-details/product-details.js
@@ -253,7 +253,7 @@ class ProductDetais {
           });
 
         $(".js-size-model").click(function(){
-          $(".size-modal").modal("show")
+          $(".js-size-modal-wrp").modal("show")
         })
 
 
