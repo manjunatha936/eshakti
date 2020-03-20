d9084ce94c390f85d17996e7dee9f116d9953698
unknown <vinod@lollypop.design>
2020-03-20 17:53:32 +0530

    bugs fixed update

--- src/js/common.js
+++ src/js/common.js
@@ -7,17 +7,17 @@ import "bootstrap/js/src/dropdown";
 import "bootstrap/js/src/tab";
 import "bootstrap/js/src/modal"
 
-import "../pug/index.pug";
-import "../pug/repeat-customer.pug";
-// import "../pug/about.pug";
-import "../pug/contact.pug";
-import "../pug/product-details.pug";
-// import "../pug/product-listing.pug";
-import "../pug/productlisting.pug";
-import "../pug/cart.pug";
-import "../pug/checkout.pug";
-import "../pug/order-placed.pug";
-import "../pug/account-wishlist.pug";
+// import "../pug/index.pug";
+// import "../pug/repeat-customer.pug";
+// // import "../pug/about.pug";
+// import "../pug/contact.pug";
+// import "../pug/product-details.pug";
+// // import "../pug/product-listing.pug";
+// import "../pug/productlisting.pug";
+// import "../pug/cart.pug";
+// import "../pug/checkout.pug";
+// import "../pug/order-placed.pug";
+// import "../pug/account-wishlist.pug";
 
 import HeaderInteractions from "./components/header";
 const headerfns = new HeaderInteractions();
