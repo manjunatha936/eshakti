64b8f81f396a54eefe581add9af413c04c61ea7a
Amit NB <amit@lollypop.design>
2020-04-01 12:42:15 +0530

    acoounts personal gifts

--- src/js/accounts-personal.js
+++ src/js/accounts-personal.js
@@ -10,14 +10,18 @@ $('.js-profile-edit').click(function(e){
     });
 });
 
-$('.js-profile-save').click(function(e){ 
-    e.preventDefault();   
-    $('.screen-edit').fadeOut("fast", function(){
-        $('.screen-info').fadeIn('slow');
-    });
-});
+// $('.js-profile-save').click(function(e){ 
+//     e.preventDefault();   
+//     $('.screen-edit').fadeOut("fast", function(){
+//         $('.screen-info').fadeIn('slow');
+//     });
+// });
 
 $(".password-popup .form-button").on('click', function () {
+    $(this).parents('form').parsley().validate();  
+});
+
+$(".js-profile-save").on('click', function () {
     $(this).parents('form').parsley().validate();  
 });
 
