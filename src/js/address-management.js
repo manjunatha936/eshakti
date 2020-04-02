733976a630ec50b4d9bf08b9487b4213797b28e0
Amit NB <amit@lollypop.design>
2020-04-03 05:10:04 +0530

    bug fixes

--- src/js/address-management.js
+++ src/js/address-management.js
@@ -1,2 +1,9 @@
 import $ from 'jquery'
 import 'parsleyjs';
+
+$(".js-add-address").on('click', function () {
+    console.log("in");
+    $('.old-address').fadeOut("fast", function(){
+        $('.form-element-container').fadeIn('slow');
+    });
+});
--- src/js/components/address-popup.js
+++ src/js/components/address-popup.js
@@ -21,13 +21,9 @@ class addressPopup {
                 }
             });
             
-            // $('.form-address').parsley();
 
             $('.form-address .address-btn').on('click', function () {
                 console.log("in");
-                // var block = $(".form-address .js-to-validate").find("input");
-                // $('.form-address').parsley().validate(block);
-
                 $('.form-address .js-to-validate :input:not(:button), .form-address .js-to-validate select').each(function (index, value) { 
                     $(this).parsley().validate();
                   });
@@ -36,10 +32,6 @@ class addressPopup {
      
         });
 
-
-        $(".js-help-submit").on('click', function () {
-            $(this).parents('form').parsley().validate();  
-        });
     }
 }
 
