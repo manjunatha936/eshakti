156cc456ccfe0059ab0725307bcef968d0c66e13
Amit NB <amit@lollypop.design>
2020-04-06 19:20:36 +0530

    return form validation

--- src/js/accounts-orders-return.js
+++ src/js/accounts-orders-return.js
@@ -3,35 +3,35 @@ import "bootstrap/js/src/modal";
 import 'parsleyjs';
 
 
-$(document).ready(function(){
-    $(document).on('click', '.order-content__details .cart-size a:not(.active)', function(e) {
+$(document).ready(function () {
+    $(document).on('click', '.order-content__details .cart-size a:not(.active)', function (e) {
         e.preventDefault();
         var el = $('.order-content__details .custom-size'),
             curHeight = el.height(),
             autoHeight = el.css('height', 'auto').height();
-        el.height(curHeight).animate({height: autoHeight}, 300);
+        el.height(curHeight).animate({ height: autoHeight }, 300);
         $(this).addClass('active').text('Show less');
     });
-    
-    $(document).on('click', '.order-content__details .cart-size a.active', function(e) {
+
+    $(document).on('click', '.order-content__details .cart-size a.active', function (e) {
         e.preventDefault();
         var el = $('.order-content__details .custom-size');
-        el.animate({height: "36px"}, 300);
+        el.animate({ height: "36px" }, 300);
         $(this).removeClass('active').text('Show more');
     });
 
-    $('.order-content__check input[type="checkbox"]').on('change', function(){
+    $('.order-content__check input[type="checkbox"]').on('change', function () {
         $(this).parents('.acc-orders__cardbox').find('.order-content__returnbox').slideToggle().addClass("js-return-validate");
     });
 
-    $(".js-return-modal").click(function(){
+    $(".js-return-modal").click(function () {
         $("#return-popup").modal("show")
     });
 
     $('.js-return-form .js-return-submit').on('click', function () {
-        console.log("in");
-        $('.js-return-form .js-return-validate :input:not(:button), .js-return-form .js-return-validate select').each(function (index, value) { 
+        $(".js-return-form").parsley();
+        $('.js-return-form .js-return-validate :input:not(:button), .js-return-form :input[name="product-item"]').each(function (index, value) {
             $(this).parsley().validate();
-          });
+        });
     });
 })
