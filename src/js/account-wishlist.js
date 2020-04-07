99e8e21fe2464b9ba1bf091a32185ccc7645899e
Amit NB <amit@lollypop.design>
2020-04-07 11:53:57 +0530

    bug fixes

--- src/js/account-wishlist.js
+++ src/js/account-wishlist.js
@@ -14,23 +14,26 @@ var wishlistSlider = new Swiper('.js-wishlist-slider', {
     grabCursor: false,
     loop: false,
     watchOverflow: true,
-    navigation: {
-        nextEl: '.left-arrow',
-        prevEl: '.right-arrow',
-    },
+    navigation: false,
       // Responsive breakpoints
     breakpoints: {
         // when window width is >= 320px
         320: {
-        slidesPerView: 1.3,
+        slidesPerView: 1.4,
         },
         // when window width is >= 480px
         480: {
-        slidesPerView: 1.3,
+        slidesPerView: 1.5,
         },
         // when window width is >= 768px
         768: {
         slidesPerView: "auto",
+        },
+        992: {
+            // navigation: {
+            //     nextEl: '.left-arrow',
+            //     prevEl: '.right-arrow',
+            // },
         }
     }
 });
